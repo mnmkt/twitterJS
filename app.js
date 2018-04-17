255f09affe5a7016114ff3f20196834afbbc71e7
Minami Kato <minamikato@gmail.com>
2018-04-17 19:35:28 -0400

    set middlewares

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+
+const logger = morgan('dev');
 
 
-app.use(function (req, res, next) {
+// nunjucks boilerplate:
+nunjucks.configure('views', {noCache: true}); // one extra step in nunjucks, to tell it what folder our index.html file lives in
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
 
-})
+app.use(morgan('dev'));
+
+//app.use takes middleware function
+// app.use(function(req, res, next){
+
+// });
+
+app.get('/', function(req, res){
+  res.send('you got the root route')
+});
 
 
-app.get('/', (req, res) => res.send('Hello world!'))
+app.get('/news', function(req, res, next){
+  res.jason({ name: 'newsRoute', date:12345 });
+});
 
-// app.get('/news', (req, res) => res.send('This is news'))
+// app.use(function(req, res, next){
 
-app.listen(3000, ()=> console.log('server listening'))
+// })
+
+// app.listen(1337, function(){
+//   response.send('here is some html')
+// })
+
