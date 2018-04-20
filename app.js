c6813f9a6c97c653fe78704756a705633bb9f8a0
Minami Kato <minamikato@gmail.com>
2018-04-19 21:27:57 -0400

    showing tweet text

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 const routes = require('./routes');
+const fs = require('fs');
+const path = require('path');
+const mime = require('mime');
 
 const logger = morgan('dev');
 
@@ -14,6 +17,20 @@ app.engine('html', nunjucks.render);
 
 //app.use = middleware function
 app.use(morgan('dev'));
+
+//the typical way to use express middleware;
+app.use(express.static(__dirname + '/public'));
+
+//Same as the line above
+// app.use(function(req, res, next){
+//   const mimeType = mime.lookup(req.path);
+//   fs.readFile('./public' + req.path, funciton(err, fileBuffer){
+//     if(err) return next();
+//     res.header('Content-Type', mimeType);
+//     res.send(fileBuffer);
+//   })
+// })
+
 app.use('/', routes);
 
 
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ router.get('/', function(req, res, next){
   res.render('index', { title: 'Twitter.js', tweets: tweets });
 });
 
+router.get('/stylesheets/style.css', function(req, res, next){
+  res.sendFile('/stylesheets/style.css', {root: __dirname + '/../public'});
+});
 
 module.exports = router;
 
